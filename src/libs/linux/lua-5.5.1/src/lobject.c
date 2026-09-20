5083ba17c74fdfbe54b3e4092d9c6738f4f210c0
Humberto Dias <hldtux@gmail.com>
2026-09-20 12:13:30 -0300

    Apply Lua 0b patch in Dockerfiles and keep 5.5.1 upstream

--- src/libs/linux/lua-5.5.1/src/lobject.c
+++ src/libs/linux/lua-5.5.1/src/lobject.c
@@ -350,14 +350,6 @@ static const char *l_str2int (const char *s, lua_Integer *result) {
       empty = 0;
     }
   }
-  else if (s[0] == '0' &&
-      (s[1] == 'b' || s[1] == 'B')) {  /* binary? */
-    s += 2;  /* skip '0b' */
-    for (; *s == '0' || *s == '1'; s++) {
-      a = (a << 1) | cast_uint(*s - '0');
-      empty = 0;
-    }
-  }
   else {  /* decimal */
     for (; lisdigit(cast_uchar(*s)); s++) {
       int d = *s - '0';
